79c6a5e525958423ec343c00c14976fe4076450d
Luke Sperkowski <lukesperk@gmail.com>
2023-12-18 10:31:14 -0600

    handlers for login built in AuthenticationController

--- src/main/java/com/codewranglers/workflowmanager/controllers/AuthenticationController.java
+++ src/main/java/com/codewranglers/workflowmanager/controllers/AuthenticationController.java
@@ -2,8 +2,16 @@ package com.codewranglers.workflowmanager.controllers;
 
 import com.codewranglers.workflowmanager.models.User;
 import com.codewranglers.workflowmanager.models.data.UserRepository;
+import com.codewranglers.workflowmanager.models.dto.LoginFormDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -32,6 +40,35 @@ public class AuthenticationController {
         }
 
         return userOpt.get();
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model, HttpSession session) {
+        model.addAttribute(new LoginFormDTO());
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO, Errors errors, HttpServletRequest request) {
+
+        if (errors.hasErrors()) {
+            return "login";
+        }
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        String password = loginFormDTO.getPassword();
+
+        if (theUser == null || !theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "login.invalid", "Incorrect username/password combination, please try again.");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(),theUser);
+        return "redirect:/dashboard";
+
+
     }
 
 
