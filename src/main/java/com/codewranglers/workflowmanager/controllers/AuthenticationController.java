da29e08075a05a9baa1a985826f7b84f9df33ecf
Luke Sperkowski <lukesperk@gmail.com>
2023-12-18 10:48:49 -0600

    tweaks to AuthenticationController and commented out the handler for the create user form until it can be reworked with current User model

--- src/main/java/com/codewranglers/workflowmanager/controllers/AuthenticationController.java
+++ src/main/java/com/codewranglers/workflowmanager/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
+@Controller
 public class AuthenticationController {
 
     @Autowired
