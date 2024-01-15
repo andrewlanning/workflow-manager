1b522ddfe5d7ec5eef9d72a6ef2a4ff69bbfa3fb
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2024-01-15 12:18:15 -0600

    refactored all Jobs sub-features(edit, update step, etc.)

--- src/main/java/com/codewranglers/workflowmanager/controllers/ProductController.java
+++ src/main/java/com/codewranglers/workflowmanager/controllers/ProductController.java
@@ -134,7 +134,7 @@ public class ProductController {
             }
             productRepository.save(product);
         }
-        return "redirect:/product";
+        return "redirect:/manager/product";
     }
 
     @GetMapping("/delete/{productId}")
