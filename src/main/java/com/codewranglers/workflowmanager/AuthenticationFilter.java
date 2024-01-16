d61bbdee7c806e618ca3e963723c99fdb5620662
Luke Sperkowski <lukesperk@gmail.com>
2024-01-16 14:29:46 -0600

    added /images to whitelist

--- src/main/java/com/codewranglers/workflowmanager/AuthenticationFilter.java
+++ src/main/java/com/codewranglers/workflowmanager/AuthenticationFilter.java
@@ -17,7 +17,7 @@ public class AuthenticationFilter implements HandlerInterceptor {
     @Autowired
     AuthenticationController authenticationController;
 
-    private static final List<String> whitelist = Arrays.asList("/login", "/logout", "/index", "/unauthorized", "/error");
+    private static final List<String> whitelist = Arrays.asList("/login", "/logout", "/index", "/unauthorized", "/error", "/images");
 
     private static boolean isWhitelisted(String path) {
         for (String pathRoot : whitelist) {
